f850ec21d99c63be32d612d89c1aed1884106a73
ardev24 <f20191044h@alumni.bits-pilani.ac.in>
2025-02-19 03:39:43 +0530

    backend build for locations

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, set, push } from "firebase/database";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyA7xdcHY8yKEayYf1erqr33UK9wfZep6LE",
-  authDomain: "car-location-app-e916d.firebaseapp.com",
-  databaseURL: "https://car-location-app-e916d-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "car-location-app-e916d",
-  storageBucket: "car-location-app-e916d.firebasestorage.app",
-  messagingSenderId: "807482328707",
-  appId: "1:807482328707:web:26659438cb52f3a0cafdb6",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-HW4KPRMSFK"
 };
 
